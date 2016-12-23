6f2c49ef50ac3a057dd297c8446c515eabef3c6f
Dale Dobson <ddobson445@gmail.com>
2016-12-22 21:17:26 -0500

    Add cards with JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
+document.addEventListener('DOMContentLoaded', function(){
+      createCards();
+});
+
+function createCards() {
+  for (var i = 0; i < 4; i++) {
+    var gameBoard = document.getElementById('game-board');
+    var card = document.createElement("div");
+    card.className = "card";
+    gameBoard.appendChild(card);
+  }
+}
+
 var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
   alert('Sorry, try again.');
 } else if (cardOne === cardTwo) {
   alert('You found a match!');
 } else if (cardThree !== cardFour) {
   alert('You found a match!');
-}
+}*/
